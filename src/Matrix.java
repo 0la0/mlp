cc77af1a41796a47f648b4c6eb589dbeb9f984ee
Luke Anderson <luke.kansas@gmail.com>
2017-05-01 21:08:34 -0500

    uncommited changes

--- src/Matrix.java
+++ src/Matrix.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Matrix {
 
